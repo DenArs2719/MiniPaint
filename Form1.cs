6b86007eb8d2b5a3a8f24b3b53efee1d78a8f998
DenArs2719 <yevseyenkod@mail.ru>
2020-04-20 11:35:52 +0200

    Obsluga metody Mouse Move i tworzenie Pena

--- Form1.cs
+++ Form1.cs
@@ -14,11 +14,16 @@ namespace MiniPaint
 {
     public partial class Form1 : Form
     {
+        Graphics graphics;
+        Point tempPoint; ///punkt pomocniczy
+        Pen myPen; 
         public Form1()
         {
             InitializeComponent();
             openFileDialog.Filter = "Grafika BMP|*.bmp|Grafika PNG|*.png|Grafika JPG|*.jpg";
             saveFileDialog.Filter = "Grafika BMP|*.bmp|Grafika PNG|*.png|Grafika JPG|*.jpg";
+            myPen = new Pen(Color.Red, 5); ///inicjalizacja Pena
+            myPen.EndCap = myPen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
         }
 
         private void openToolStripMenuItem_Click(object sender, EventArgs e)
@@ -26,6 +31,7 @@ namespace MiniPaint
             if(openFileDialog.ShowDialog() == DialogResult.OK) ///sprawdziamy rezultat,został znalieziony poprawny plik
             {
                 pictureBoxMyImage.Image = Image.FromFile(openFileDialog.FileName); ///nazwa naszego pliku 
+                graphics = Graphics.FromImage(pictureBoxMyImage.Image); ///z naszego obrazka tworzymy grafikę
             }
         }
 
@@ -57,20 +63,30 @@ namespace MiniPaint
         {
             if(e.Button == MouseButtons.Left)
             {
-                Graphics graphics = Graphics.FromImage(pictureBoxMyImage.Image); ///z naszego obrazka tworzymy grafikę
-                graphics.DrawEllipse(new Pen(Color.Red), e.X,e.Y,20,20);///w mejsce gzie kliknemy będzie narysowany ellipse
-                pictureBoxMyImage.Refresh(); ///odresowanie image  
+                //graphics.DrawEllipse(new Pen(Color.Red), e.X,e.Y,20,20);///w mejsce gzie kliknemy będzie narysowany ellipse
+                //pictureBoxMyImage.Refresh(); ///odresowanie image  
+                tempPoint = e.Location; ///zapamientujemy punkt
             }
         }
 
         private void pictureBoxMyImage_MouseMove(object sender, MouseEventArgs e)
         {
-
+            if(e.Button == MouseButtons.Left)
+            {
+                graphics.DrawLine(myPen, tempPoint, e.Location);
+                pictureBoxMyImage.Refresh();
+            }
+            tempPoint = e.Location;
+           
         }
 
         private void pictureBoxMyImage_MouseUp(object sender, MouseEventArgs e)
         {
-
+           /*( if (e.Button == MouseButtons.Left)
+            {
+                //graphics.DrawLine(new Pen(Color.Red, 5), tempPoint, e.Location);
+                pictureBoxMyImage.Refresh();
+            }*/
         }
     }
 }
